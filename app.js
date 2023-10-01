99b439cebc0b782a54ee84fc9678043a55622d3b
Ushie Francis <ushiefrancis001@gmail.com>
2023-10-02 00:09:56 +0100

    a simple landing page for practice

---
+++ app.js
@@ -0,0 +1,18 @@
+// Write all your javascript here
+
+// make use the following 
+// *Arrow functions
+// *addEventListener
+// *Onclick
+// *.Map()
+
+// On the NAVBAR, add a color to each list item
+// Increase the font size with either px, rem or em
+
+// Play around with the buttons (Learn More, Subscribe)
+// you may decide to add more buttons from the DOM here
+
+// NB: after reading this comment you may decide to delete
+
+// Happy coding bro💻
+
